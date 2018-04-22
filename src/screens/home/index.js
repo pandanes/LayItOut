a9e20f8dc0090915e9a0da6a50719fe15a5fe607
Danes <pandanes@danes-mbp.local>
2018-04-22 23:35:16 +0700

    add info section / card and react navigation

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -6,7 +6,7 @@ import { Text, TextInput, TouchableOpacity, Image, View, StyleSheet, ScrollView,
 export default class Home extends Component {
 
   render() {
-    
+
     return (
       <View style = {styles.container}>
         <ScrollView>
@@ -14,13 +14,15 @@ export default class Home extends Component {
             <Text style ={styles.pageTitle}>HOME</Text>
           </View>
           <ScrollView horizontal = {true} showsHorizontalScrollIndicator = {false}>
-            <View style = {styles.promoCard}></View>
+            <View style = {styles.promoCard}>
+               <Image style = {styles.promoImage} source = {{uri: 'https://i.pinimg.com/564x/06/28/f2/0628f22d6899be1b2a5f31a221b2450f.jpg'}} />
+            </View>
             <View style = {styles.promoCard}>
               <WebView style={styles.WebViewStyle} source={{uri: 'https://Google.com'}}/>
             </View>
             <View style = {styles.promoCard}></View>
           </ScrollView>
-          <View style = {styles.brandsWrapper}>
+          <View style = {styles.cardWrapper}>
             <View style = {styles.brandsTitle}>
               <Text style = {styles.headingText}>TOP BRANDS</Text>
               <Text style = {styles.linkText}>View all brands</Text>  
@@ -35,8 +37,16 @@ export default class Home extends Component {
               <Image style = {styles.brandImage} source = {{uri: 'https://i.pinimg.com/564x/dd/3b/32/dd3b32aaa16560b410837bfc03aba9fc.jpg'}} />
               <Image style = {styles.brandImage} source = {{uri: 'https://i.pinimg.com/564x/27/d7/67/27d767b54f64808d3373428811a92726.jpg'}} />
             </View>
-            
           </View>
+          <View style = {styles.cardWrapper}>
+              <View style = {styles.infoWrapper}>
+                <Image style = {styles.infoIcon} source = {{uri: 'https://image.flaticon.com/icons/png/256/838/838618.png'}} />
+                <View style = {styles.infoDetail}>
+                  <Text style = {[styles.headingText, {marginBottom: 10}]}>LOREM IPSUM</Text>
+                  <Text style = {styles.bodyText}>Fusce vehicula dolor arcu, sit amet blandit dolor mollis nec. Donec viverra eleifend lacus.</Text>
+                </View>
+              </View>
+            </View>
         </ScrollView>
       </View>
     );
@@ -48,13 +58,13 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   container: {
-    backgroundColor: 'rgba(250,250,250,1.0)'
+    backgroundColor: '#f2f2f2'
   },
   topBar: {
     justifyContent: 'center',
     alignItems: 'center',
-    height: 80,
-    backgroundColor: 'rgba(247, 112, 98, 1.0)'
+    height: 60,
+    backgroundColor: '#4A90E2'
   },
   pageTitle: {
     fontFamily: 'Roboto-Medium',
@@ -69,10 +79,17 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginLeft: 20,
     borderRadius: 6,
-    backgroundColor: 'purple'
+    backgroundColor: '#4A90E2'
   },
-  brandsWrapper: {
+  promoImage: {
+    width: 350,
+    height: 180,
+    borderRadius: 6,
+    opacity: 0.5
+  },
+  cardWrapper: {
     padding: 20,
+    marginBottom: 20,
     backgroundColor: 'white',
   },
   brandsTitle: {
@@ -100,5 +117,25 @@ const styles = StyleSheet.create({
     width: 110,
     height: 110,
     borderRadius: 6
+  },
+  infoWrapper: {
+    flexDirection: 'row'
+  },
+  infoIcon: {
+    width: 90,
+    height: 90
+  },
+  infoDetail: {
+    paddingLeft: 20,
+    paddingRight:20,
+  },
+  bodyText: {
+    width: 280,
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+    color: 'rgba(0,0,0,0.54)',
+    letterSpacing: 1.2,
+    lineHeight: 20,
+    textAlign: 'justify' //only iOS, fallback 'left' on android
   }
 });
